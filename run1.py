723fa08ac95b5ebd7d30137f66468967b05bc0c6
brenzonel <bran.alan@hotmail.com>
2024-03-14 16:04:06 -0600

    API en flask para geolocalizacion ip

---
+++ run1.py
@@ -0,0 +1,33 @@
+# import folium package
+import folium
+import geocoder
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/mappp')
+def maps1():
+    g = geocoder.ip('me')
+    print(g.latlng)
+    #print(g.latlng[0])
+
+    g1 = 'https://www.google.com.mx/maps/search/'+ str(g.latlng[0]) + ',' + str(g.latlng[1])
+
+    #print(g1)
+
+    #my_map3 = folium.Map(location = [g.latlng[0], g.latlng[1]],
+    #                                        zoom_start = 15)
+    
+    # Pass a string in popup parameter
+    #folium.Marker([g.latlng[0], g.latlng[1]],
+    #            popup = ' Geeksforgeeks.org ').add_to(my_map3)
+    
+    
+    #my_map3.save(" my_map3.html ")
+    return g1
+
