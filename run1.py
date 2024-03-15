5f2e4228685771b9b9cf3ba15d33d8243ef7c9e4
brenzonel <113213176+brenzonel@users.noreply.github.com>
2024-03-14 18:51:18 -0600

    Update run1.py

--- run1.py
+++ run1.py
@@ -16,10 +16,6 @@ def maps1():
     print(g.latlng)
     #print(g.latlng[0])
 
-    g1 = 'https://www.google.com.mx/maps/search/'+ str(g.latlng[0]) + ',' + str(g.latlng[1])
-
-    #print(g1)
-
     #my_map3 = folium.Map(location = [g.latlng[0], g.latlng[1]],
     #                                        zoom_start = 15)
     
@@ -29,5 +25,5 @@ def maps1():
     
     
     #my_map3.save(" my_map3.html ")
-    return g1
+    return g.latlng
 
